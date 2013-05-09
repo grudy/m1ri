a4bcde04981e433cdce7e757bf7b983d962de741
grudy <williamandrewalumbaugh@gmail.com>
2013-05-09 15:42:03 -0500

    Update addingproofofconcept.c

--- addingproofofconcept.c
+++ addingproofofconcept.c
@@ -1,10 +1,25 @@
-//
-// main.c
-// proofofconcept of the addition logic used in the paper published by
+//// proofofconcept of the addition logic used in the paper published by
 // TOMAS J. BOOTHBY AND ROBERT W. BRADSHAW "BITSLICING AND THE METHOD OF FOUR
 // RUSSIANS OVER LARGER FINITE FIELDS"
 //
-// William Andrew Alumbaugh
+/*Copyright 2013 William Andrw Alumbaugh <williamandrewalumbaugh@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+// Addingproofofconcept.c
+
 
 
 #include <stdio.h>
