d16e3703e65647df9ae7c807746be2ff560e2b24
grudy <williamandrewalumbaugh@gmail.com>
2013-06-06 03:32:14 -0500

    notes

--- m1rigf3/strassen.h
+++ m1rigf3/strassen.h
@@ -1,11 +1,24 @@
 //
-//  strassen.h
-//  m1rigf3
+// strassen.h
 //
-//  Created by grudy on 5/25/13.
-//  Copyright (c) 2013 William Alumbaugh. All rights reserved.
+//// computions for small matrices over gf3
+// TOMAS J. BOOTHBY AND ROBERT W. BRADSHAW "BITSLICING AND THE METHOD OF FOUR
+// RUSSIANS OVER LARGER FINITE FIELDS"
 //
-
+/*Copyright 2013 William Andrew Alumbaugh <williamandrewalumbaugh@gmail.com>
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 #ifndef m1rigf3_strassen_h
 #define m1rigf3_strassen_h
 
