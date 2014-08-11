444fa58dc72a4198d5fef69f0414d35ad07a569b
william <williamandrewalumbaugh@gmail.com>
2014-08-10 23:05:14 -0500

    code style

--- tests/test_m3d.c
+++ tests/test_m3d.c
@@ -27,56 +27,56 @@ Copyright 2013 William Andrew Alumbaugh <williamandrewalumbaugh@gmail.com>
  int main(int argc, const char * argv[])
 {
   /*
-   [a][b]
-   [c][d]
-   
-  */
-  m3d_t * o, *ab, *cd, *abcd, *v; 
-  m3_slice * s;  
+	 [a][b]
+	 [c][d]
+	 
+	*/
+	m3d_t * o, *ab, *cd, *abcd, *v; 
+	m3_slice * s;  
  
-  o   = m3d_create( 128, 128);
-  m3d_rand(o);
+	o   = m3d_create( 128, 128);
+	m3d_rand(o);
 		/*    */
-  
-   s = m3d_quarter( o);
-  
-  
-  m3d_specs(s->row[0]);
-  m3d_print(s->row[0]);
-  m3d_print(s->row[1]);
-  m3d_print(s->row[2]);
-  m3d_print(s->row[3]);
+	
+	 s = m3d_quarter( o);
+	
+	
+	m3d_specs(s->row[0]);
+	m3d_print(s->row[0]);
+	m3d_print(s->row[1]);
+	m3d_print(s->row[2]);
+	m3d_print(s->row[3]);
 
  
-  ab = m3d_concat(ab, s->row[0], s->row[1]);
-  cd = m3d_concat(cd, s->row[2], s->row[3]);
-  
-  m3d_specs(s->row[0]);
-  abcd = m3d_stack(abcd,  ab, cd);
-  m3d_specs(o);
-  m3d_specs(abcd);
-  
-  printf("\nab\n");
-  m3d_print(ab);
-  printf("\ncd\n");
-  m3d_print(cd);
-  printf("\n abcd \n");
-  m3d_print(abcd);
-  printf("\no\n");
-  m3d_print(o);
-  if(!m3d_equal(o, abcd))
-  {
-     printf("\n o and abcd not equal, stack and concat test failed \n");
-  	 return 1;
-  }
-  
-  v = m3d_create( 4, 4);
-  
-  m3d_rand(v);
-  m3d_print(v);
+	ab = m3d_concat(ab, s->row[0], s->row[1]);
+	cd = m3d_concat(cd, s->row[2], s->row[3]);
+	
+	m3d_specs(s->row[0]);
+	abcd = m3d_stack(abcd,  ab, cd);
+	m3d_specs(o);
+	m3d_specs(abcd);
+	
+	printf("\nab\n");
+	m3d_print(ab);
+	printf("\ncd\n");
+	m3d_print(cd);
+	printf("\n abcd \n");
+	m3d_print(abcd);
+	printf("\no\n");
+	m3d_print(o);
+	if(!m3d_equal(o, abcd))
+	{
+	   printf("\n o and abcd not equal, stack and concat test failed \n");
+		 return 1;
+	}
+	
+	v = m3d_create( 4, 4);
+	
+	m3d_rand(v);
+	m3d_print(v);
 
-  m3d_colswap(v, 1, 2);
-  m3d_print(v);
+	m3d_colswap(v, 1, 2);
+	m3d_print(v);
 
 
   /*
@@ -88,22 +88,19 @@ Copyright 2013 William Andrew Alumbaugh <williamandrewalumbaugh@gmail.com>
     w6 = w0*w4;
   */ 
   
-  m3d_t * w0, *w1,* w2,* w3, *w4,* w5,* w6;
+  	m3d_t * w0, *w1,* w2,* w3, *w4,* w5,* w6;
 
+	w0 = m3d_create(512, 512);
+  	w1 = m3d_create(512, 512);
+	w2 = m3d_create(512, 512);
   
-
-  w0 = m3d_create(512, 512);
-  w1 = m3d_create(512, 512);
-  w2 = m3d_create(512, 512);
-  
-  m3d_rand(w0);
-  m3d_rand(w1);
-  m3d_rand(w2);
-  
-  w3 = m3d_classic_mul(w3, w0, w1);
-  w4 = m3d_classic_mul(w4, w1, w2);
-  w5 = m3d_classic_mul(w5, w3, w2);
-  w6 = m3d_classic_mul(w6, w0, w4);
+  	m3d_rand(w0);
+  	m3d_rand(w1);
+	m3d_rand(w2);
+  	w3 = m3d_classic_mul(w3, w0, w1);
+	w4 = m3d_classic_mul(w4, w1, w2);
+	w5 = m3d_classic_mul(w5, w3, w2);
+	w6 = m3d_classic_mul(w6, w0, w4);
    	
 
   /*	
@@ -185,6 +182,7 @@ Copyright 2013 William Andrew Alumbaugh <williamandrewalumbaugh@gmail.com>
 	m3d_rand(a);
 	m3d_copy(b, a);
 	
+	if
 	
 	
 	
