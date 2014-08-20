b83d3a84f19a5815646cd0cd19937f68f01c5afa
william <williamandrewalumbaugh@gmail.com>
2014-08-20 12:44:02 -0500

    uncommenting
    was commented for a test

--- m1ri/m1ri_strassen.c
+++ m1ri/m1ri_strassen.c
@@ -278,7 +278,7 @@ m3d_t *  m3d_strassen(m3d_t *c,const m3d_t  *a,const m3d_t   *b)
 		padded_b = m3d_create(acbr, bccc);
 		padded_c = m3d_create(arcr, bccc);
 		padded_a = m3d_copy(padded_a, a);
-		/*padded_b = m3d_copy(padded_b, b);
+		padded_b = m3d_copy(padded_b, b);
 		padded_c = m3d_copy(padded_c, c);
 	
 		m3d_qrt_mul(padded_c, padded_a, padded_b); 
@@ -286,7 +286,7 @@ m3d_t *  m3d_strassen(m3d_t *c,const m3d_t  *a,const m3d_t   *b)
 		m3d_free(padded_a);
 		m3d_free(padded_b);
 		m3d_free(padded_c);
-		*/
+		
 		
 	}
 	
